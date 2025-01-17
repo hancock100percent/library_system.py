4c7a02767594329268cf26cf2e95d97787ef5bf0
Michael Morera <mikemorera@yahoo.com>
2025-01-16 18:04:35 -0800

    add working version of library system before automation

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from library_system import LibrarySystem
 # Start up our library system
 library = LibrarySystem()
 
-# Add a book
+# Add some books
 library.add_book("Harry Potter", "J.K. Rowling", "12345")
+library.add_book("Lord of the Rings", "J.R.R. Tolkien", "67890")
+library.add_book("The Great Gatsby", "F. Scott Fitzgerald", "11111")
 
 # Add a member
 library.add_member("Bob", "bob@email.com")
